a6ba2e92e5cdd25a425cbe9f8e3c846cc0a16587
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-05-14 10:56:48 +0200

    Aggiunge InvalidCommandException.java

---
+++ src/main/java/it/uniba/app/exception/InvalidCommandException.java
@@ -0,0 +1,10 @@
+package it.uniba.app.exception;
+
+/**
+ * <noECB>
+ * <p>
+ * Rappresenta una situazione in cui l'utente ha inserito un comando invalido.
+ */
+
+public class InvalidCommandException extends Exception {
+}
