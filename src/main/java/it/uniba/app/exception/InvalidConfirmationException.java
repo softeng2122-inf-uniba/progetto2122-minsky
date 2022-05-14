29cda838af5d79f4a4af2525e626e63a19133869
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-05-14 12:35:00 +0200

    Aggiunge classi per chiedere conferma all'utente

---
+++ src/main/java/it/uniba/app/exception/InvalidConfirmationException.java
@@ -0,0 +1,11 @@
+package it.uniba.app.exception;
+
+/**
+ * <noECB>
+ * <p>
+ * Rappresenta una situazione in cui l'utente
+ * non ha risposto correttamente ad una richiesta di conferma.
+ */
+
+public class InvalidConfirmationException extends Exception {
+}
