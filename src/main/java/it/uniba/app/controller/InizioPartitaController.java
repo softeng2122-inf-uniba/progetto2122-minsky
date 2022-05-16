ec0f9134105f402e34bef845615e766effa7ec3e
Mario Venezia <mvenezia00@hotmail.it>
2022-05-16 21:51:10 +0200

    Create eccezioni per InizioPartitaController

--- src/main/java/it/uniba/app/controller/InizioPartitaController.java
+++ src/main/java/it/uniba/app/controller/InizioPartitaController.java
@@ -1,5 +1,10 @@
 package it.uniba.app.controller;
 
+import it.uniba.app.InizioPartitaBoundary;
+import it.uniba.app.exception.ParolaSegretaMancanteException;
+import it.uniba.app.exception.PartitaInCorsoException;
+import it.uniba.app.wordle.Partita;
+
 /**
  * <Control>
  * <p>
@@ -10,6 +15,16 @@ public class InizioPartitaController implements Controller {
 
     @Override
     public void control(String[] args) {
+        InizioPartitaBoundary inizioPartitaBoundary = new InizioPartitaBoundary();
 
+        try {
+            Partita.iniziaNuovaPartita();
+
+            inizioPartitaBoundary.showEmptyGrid();
+        } catch (PartitaInCorsoException e) {
+            inizioPartitaBoundary.showGameRunningError();
+        } catch (ParolaSegretaMancanteException e) {
+            inizioPartitaBoundary.showMissingSecretWord();
+        }
     }
 }
