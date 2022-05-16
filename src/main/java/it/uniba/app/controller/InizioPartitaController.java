9a7b5b9b660f2c8b56622f6281e71540706e6f56
Mario Venezia <mvenezia00@hotmail.it>
2022-05-16 18:46:42 +0200

    Aggiunge classe control InizioPartitaController

---
+++ src/main/java/it/uniba/app/controller/InizioPartitaController.java
@@ -0,0 +1,15 @@
+package it.uniba.app.controller;
+
+/**
+ * <Control>
+ * <p>
+ * Gestisce l'avvio di una partita di Wordle.
+ */
+
+public class InizioPartitaController implements Controller {
+
+    @Override
+    public void control(String[] args) {
+
+    }
+}
