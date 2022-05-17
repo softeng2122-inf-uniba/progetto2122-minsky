cc9a597fd186df6ccd35656482cc7b386d2862ac
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-05-17 09:13:31 +0200

    Aggiunge classe SelezioneParolaSegretaController

---
+++ src/main/java/it/uniba/app/controller/SelezioneParolaSegretaController.java
@@ -0,0 +1,14 @@
+package it.uniba.app.controller;
+
+/**
+ * <Control>
+ * <p>
+ * Gestisce il processo di selezione di una nuova parola segreta.
+ */
+
+public class SelezioneParolaSegretaController implements Controller {
+    @Override
+    public void control(String[] args) {
+
+    }
+}
