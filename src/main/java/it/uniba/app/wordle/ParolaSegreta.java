8f2c79d89dc9df8909dc605310aa7ac4f8bd5d0d
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-05-17 14:40:51 +0200

    Aggiunge metodo impostaAttualeParolaSegreta
    Nella classe ParolaSegreta

--- src/main/java/it/uniba/app/wordle/ParolaSegreta.java
+++ src/main/java/it/uniba/app/wordle/ParolaSegreta.java
@@ -3,6 +3,7 @@ package it.uniba.app.wordle;
 import it.uniba.app.exception.LetteraInvalidaException;
 import it.uniba.app.exception.ParolaCortaException;
 import it.uniba.app.exception.ParolaLungaException;
+import it.uniba.app.exception.PartitaInCorsoException;
 
 /**
  * <Entity>
@@ -14,7 +15,15 @@ public class ParolaSegreta extends Parola {
     private static ParolaSegreta attualeParolaSegreta;
 
     public ParolaSegreta(final String parola) throws ParolaCortaException, ParolaLungaException, LetteraInvalidaException {
-        super(parola);
+        super(parola.toLowerCase());
+    }
+
+    public static void impostaAttualeParolaSegreta(final ParolaSegreta parolaSegreta) throws PartitaInCorsoException {
+        if (Partita.getPartitaInCorso() == null) {
+            attualeParolaSegreta = parolaSegreta;
+        } else {
+            throw new PartitaInCorsoException();
+        }
     }
 
     public static ParolaSegreta getAttualeParolaSegreta() {
