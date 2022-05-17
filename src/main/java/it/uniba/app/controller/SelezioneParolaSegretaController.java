79eb911bd8ff1b380436c89c8bcc6e3cdc02d857
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-05-17 14:43:39 +0200

    Aggiunge logica per selezionare la parola segreta

--- src/main/java/it/uniba/app/controller/SelezioneParolaSegretaController.java
+++ src/main/java/it/uniba/app/controller/SelezioneParolaSegretaController.java
@@ -1,14 +1,37 @@
 package it.uniba.app.controller;
 
+import it.uniba.app.exception.LetteraInvalidaException;
+import it.uniba.app.exception.ParolaCortaException;
+import it.uniba.app.exception.ParolaLungaException;
+import it.uniba.app.exception.PartitaInCorsoException;
+import it.uniba.app.utility.SelezioneParolaSegretaBoundary;
+import it.uniba.app.wordle.ParolaSegreta;
+
 /**
  * <Control>
  * <p>
  * Gestisce il processo di selezione di una nuova parola segreta.
  */
 
-public class SelezioneParolaSegretaController implements Controller {
+public final class SelezioneParolaSegretaController implements Controller {
     @Override
-    public void control(String[] args) {
+    public void control(final String[] args) {
+        SelezioneParolaSegretaBoundary selezioneParolaSegretaBoundary = new SelezioneParolaSegretaBoundary();
 
+        try {
+            ParolaSegreta.impostaAttualeParolaSegreta(new ParolaSegreta(args[0]));
+
+            selezioneParolaSegretaBoundary.mostraOK();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            selezioneParolaSegretaBoundary.mostraErroreParolaMancante();
+        } catch (PartitaInCorsoException e) {
+            selezioneParolaSegretaBoundary.mostraErrorePartitaInCorso();
+        } catch (ParolaCortaException e) {
+            selezioneParolaSegretaBoundary.mostraErroreParolaCorta(ParolaSegreta.getLength());
+        } catch (ParolaLungaException e) {
+            selezioneParolaSegretaBoundary.mostraErroreParolaLunga(ParolaSegreta.getLength());
+        } catch (LetteraInvalidaException e) {
+            selezioneParolaSegretaBoundary.mostraErroreParolaInvalida();
+        }
     }
 }
