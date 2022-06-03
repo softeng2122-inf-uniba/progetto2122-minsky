39ef4de85b839893a3c445129b721d82528a584b
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-03 18:23:18 +0200

    Traduce Partita.java in inglese

---
+++ src/main/java/it/uniba/app/wordle/Game.java
@@ -0,0 +1,63 @@
+package it.uniba.app.wordle;
+
+import it.uniba.app.controller.attemptController;
+import it.uniba.app.exception.MissingCurrentSecretWordException;
+import it.uniba.app.exception.MissingRunningGameException;
+import it.uniba.app.exception.RunningGameException;
+
+/**
+ * <Entity>
+ * <p>
+ * A game of Wordle.
+ */
+
+public class Game {
+
+    private static final int maxGameAttempts = 6;
+    private static Game runningGame;
+    private final GameGrid gameGrid = new GameGrid();
+    private final SecretWord secretWord;
+
+    public Game(final SecretWord secretWord) {
+        this.secretWord = secretWord;
+    }
+
+    public static void startNewGame() throws RunningGameException, MissingCurrentSecretWordException {
+        if (getRunningGame() == null) {
+            if (SecretWord.getCurrentSecretWord() != null) {
+                attemptController.resetWin();
+                attemptController.resetAttemptCount();
+                attemptController.clearAttempt();
+                runningGame = new Game(SecretWord.getCurrentSecretWord());
+            } else {
+                throw new MissingCurrentSecretWordException();
+            }
+        } else {
+            throw new RunningGameException();
+        }
+    }
+
+    public static void abortRunningGame() throws MissingRunningGameException {
+        if (getRunningGame() != null) {
+            runningGame = null;
+        } else {
+            throw new MissingRunningGameException();
+        }
+    }
+
+    public static Game getRunningGame() {
+        return runningGame;
+    }
+
+    public static int getMaxGameAttempts() {
+        return maxGameAttempts;
+    }
+
+    public GameGrid getGameGrid() {
+        return gameGrid;
+    }
+
+    public SecretWord getSecretWord() {
+        return secretWord;
+    }
+}
