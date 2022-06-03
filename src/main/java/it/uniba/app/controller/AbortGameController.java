6363a56b28c0a6d67b1075f14d555d0989bec9f0
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-03 18:21:17 +0200

    Traduce AbbandonaPartitaController.java in inglese

---
+++ src/main/java/it/uniba/app/controller/AbortGameController.java
@@ -0,0 +1,37 @@
+package it.uniba.app.controller;
+
+import it.uniba.app.exception.InvalidConfirmationException;
+import it.uniba.app.exception.MissingRunningGameException;
+import it.uniba.app.utility.AbortGameBoundary;
+import it.uniba.app.utility.ConfirmationRequest;
+import it.uniba.app.wordle.Game;
+
+import java.io.IOException;
+
+/**
+ * <Control>
+ * <p>
+ * This class allows the player
+ * to abort the game currently running.
+ */
+public class AbortGameController implements Controller {
+    @Override
+    public void control(String[] args) {
+        try {
+            if (Game.getRunningGame() != null) {
+                System.out.println("Vuoi Abbandonare la partita in corso?");
+                ConfirmationRequest abortConfirmation = new ConfirmationRequest();
+                if (abortConfirmation.askUserConfirmation()) {
+                    Game.abortRunningGame();
+                    AbortGameBoundary.showGameAbortedMessage();
+                }
+            } else {
+                throw new MissingRunningGameException();
+            }
+        } catch (IOException | InvalidConfirmationException ex) {
+            System.out.println("Errore nell'input");
+        } catch (MissingRunningGameException e) {
+            AbortGameBoundary.showGameNotRunningError();
+        }
+    }
+}
