b4a7c0b67b5fc8e7c1d081d5cdb819b9e18f73c4
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-03 18:33:34 +0200

    Traduce PartitaInCorsoException.java in inglese

---
+++ src/main/java/it/uniba/app/exception/RunningGameException.java
@@ -0,0 +1,17 @@
+package it.uniba.app.exception;
+
+/**
+ * <noECB>
+ * <p>
+ * Thrown to indicate that the player tried to start a new game
+ * while another one was already running.
+ */
+
+public class RunningGameException extends Exception {
+
+    public String showMessage() {
+
+        return "Impossibile effettuare un tentativo se la partita non è in corso, per maggiori informazioni digitare /help";
+    }
+
+}
