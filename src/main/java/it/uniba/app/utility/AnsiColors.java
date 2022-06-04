fcf36215f79f831a90b40c5ad7f827912851a837
Vito Proscia <vitoproscia.1704@gmail.com>
2022-06-04 11:48:45 +0200

    Resi attributi di AnsiColors privati

--- src/main/java/it/uniba/app/utility/AnsiColors.java
+++ src/main/java/it/uniba/app/utility/AnsiColors.java
@@ -9,14 +9,14 @@ package it.uniba.app.utility;
 
 public class AnsiColors {
 
-    public static final String ANSI_BRIGHT_GREEN = "\u001b[32;1m";
-    public static final String ANSI_BRIGHT_RED = "\u001b[31;1m";
+    private static final String ANSI_BRIGHT_GREEN = "\u001b[32;1m";
+    private static final String ANSI_BRIGHT_RED = "\u001b[31;1m";
 
-    public static final String ANSI_BACKGROUND_BRIGHT_GREEN = "\u001b[42;1m";
-    public static final String ANSI_BACKGROUND_BRIGHT_YELLOW = "\u001b[43;1m";
-    public static final String ANSI_BACKGROUND_BRIGHT_GRAY = "\u001b[47;1m";
+    private static final String ANSI_BACKGROUND_BRIGHT_GREEN = "\u001b[42;1m";
+    private static final String ANSI_BACKGROUND_BRIGHT_YELLOW = "\u001b[43;1m";
+    private static final String ANSI_BACKGROUND_BRIGHT_GRAY = "\u001b[47;1m";
 
-    public static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RESET = "\u001B[0m";
 
     public static String getBrightRed() {
         return ANSI_BRIGHT_RED;
