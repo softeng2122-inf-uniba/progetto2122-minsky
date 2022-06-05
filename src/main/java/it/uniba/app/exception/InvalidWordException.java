1024f44b5a8ea33c54612b8d04c0eb85a2cc7fa0
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-05 13:47:50 +0200

    Aggiunge eccezione InvalidWordException

---
+++ src/main/java/it/uniba/app/exception/InvalidWordException.java
@@ -0,0 +1,28 @@
+package it.uniba.app.exception;
+
+/**
+ * <noECB>
+ * <p>
+ * Thrown to indicate that the user entered a {@code Word}
+ * containing non-alphabetic characters.
+ *
+ * @see it.uniba.app.wordle.Word
+ * @see it.uniba.app.wordle.Letter
+ * @see InvalidLetterException
+ */
+
+public class InvalidWordException extends Exception {
+
+    /**
+     * Constructs a new {@code InvalidWordException}
+     * with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public InvalidWordException(String message) {
+        super(message);
+    }
+}
