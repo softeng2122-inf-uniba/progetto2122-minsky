cdb3aba6a0c318a827f5321d75dd602c357b64c2
stancrini <fanfaino@gmail.com>
2022-06-05 11:59:20 +0200

    Refactoring classe ShowMessage per errori di checkstyle

--- src/main/java/it/uniba/app/controller/ShowMessage.java
+++ src/main/java/it/uniba/app/controller/ShowMessage.java
@@ -11,24 +11,33 @@ import it.uniba.app.wordle.SecretWord;
  * @see SecretWord
  */
 
-public class ShowMessage implements Controller{
-    private static final String SHOWING_MESSAGE = "Questa è la parola segreta impostata: ";
-    private static final String MISSING_WORD="\n[ERRORE]Non è stata impostata nessuna parola segreta";
-    
-    @Override
-    public void control(String[] args)  {
+public class ShowMessage implements Controller {
 
-        
-        try{
+    /** @param is a string that show an initial message. */
+    private static final String SHOWING_MESSAGE =
+    "Questa è la parola segreta impostata: ";
+
+    /** @param is a string that show a message
+     * if the current secret word is not set. */
+    private static final String MISSING_WORD =
+    "\n[ERRORE]Non è stata impostata nessuna parola segreta";
+
+    /**
+     * This method is used to display the current {@code SecretWord}.
+     */
+    @Override
+    public void control(final String[] args)  {
+        try {
             if (SecretWord.getCurrentSecretWord() != null) {
                 System.out.println(SHOWING_MESSAGE);
-                System.out.println(SecretWord.getCurrentSecretWord().toString());
+                SecretWord thisSecrW = SecretWord.getCurrentSecretWord();
+                System.out.println(thisSecrW.toString());
             } else {
                 throw new MissingCurrentSecretWordException();
-            }  
+            }
         } catch (MissingCurrentSecretWordException e) {
             System.out.println(MISSING_WORD);
         }
-         
+
     }
 }
