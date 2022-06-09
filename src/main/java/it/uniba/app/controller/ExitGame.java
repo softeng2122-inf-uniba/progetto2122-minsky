b4e7f36730fab38447f99acaea728015249728fc
Quaglia98 <vincy.q.98@gmail.com>
2022-06-09 15:41:46 +0200

    rimozione stringa e stampe interne di ExitGame

--- src/main/java/it/uniba/app/controller/ExitGame.java
+++ src/main/java/it/uniba/app/controller/ExitGame.java
@@ -13,21 +13,17 @@ import java.io.IOException;
 
 public class ExitGame implements Controller  {
 
-    private static final String EXIT_CONFIRMATION = "Sei sicuro di voler uscire da Wordle?";
-    
     ConfirmationRequest exitConfirmation = new ConfirmationRequest();
     
     
     public void control(String[] args) {
-        
-        System.out.println(EXIT_CONFIRMATION);
-        
+             
         try {
            if(exitConfirmation.askUserConfirmation()){
                System.exit(0);
            }
         } catch (IOException | InvalidConfirmationException ex) {
-            System.out.println("Errore nell'input");
+            
         }
    
    }
