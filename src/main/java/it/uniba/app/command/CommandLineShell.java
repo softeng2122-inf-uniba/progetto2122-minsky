1b7299361cdfd348a29e82ab390353b7c744c210
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-09 12:51:08 +0200

    Applica refactoring al package command

--- src/main/java/it/uniba/app/command/CommandLineShell.java
+++ src/main/java/it/uniba/app/command/CommandLineShell.java
@@ -7,7 +7,9 @@ import it.uniba.app.exception.InvalidCommandException;
 import it.uniba.app.utility.ErrorStringBuilder;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * <Boundary>
@@ -17,26 +19,81 @@ import java.io.InputStreamReader;
  */
 
 public final class CommandLineShell {
-    private static final String HELP_TIP = "[TIP] Usa /help per visualizzare la lista dei comandi disponibili.";
-    private static final String WELCOME_MESSAGE = "\n\n ================================ Benvenuto su Wordle! ================================ \n\n";
-    private static final String UNKNOWN_COMMAND_MESSAGE = "Comando inesistente.";
-    private static final String INPUT_PREFIX = "wordle:> ";
-    private static final String EXIT_MESSAGE = "/esci";
-    private static final String HELP_MESSAGE = "/help";
-    private static final String SHOW_MESSAGE = "/mostra";
-    private final CommandParser commandParser = new CommandParser();
-    private final BufferedReader commandLineInputStream = new BufferedReader(new InputStreamReader(System.in));
 
-    public CommandLineShell() {
-        registerCommands();
+    /**
+     * Welcome message displayed on application start-up.
+     */
+    private static final String WELCOME_MESSAGE =
+            "\n\n ================================ "
+                    + "Benvenuto su Wordle! "
+                    + "================================ \n\n";
+
+    /**
+     * Message suggesting how to view the list of available commands.
+     */
+    private static final String HELP_TIP =
+            "[TIP] Usa /help per visualizzare la lista dei comandi disponibili";
+
+    /**
+     * Prefix displayed before each command request or attempt.
+     */
+    private static final String INPUT_PREFIX = "wordle:> ";
+
+    /**
+     * Message shown when the user enters a non-existent command.
+     */
+    private static final String UNKNOWN_COMMAND_MESSAGE = "Comando inesistente";
+
+    /**
+     * Message shown when an unexpected error occurs
+     * during application execution.
+     */
+    private static final String UNEXPECTED_ERROR_MESSAGE =
+            "Si è verificato un errore inaspettato. "
+                    + "Comunicare al team di sviluppo il seguente testo:\n";
+
+    /**
+     * Responsible for the recognition of commands
+     * in this {@code CommandLineShell}.
+     */
+    private final CommandParser commandParser = new CommandParser();
+
+    /**
+     * Input stream from Command Line Interface.
+     */
+    private final BufferedReader commandLineInputStream = new BufferedReader(
+            new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+
+    /**
+     * Used to display unexpected errors during normal application execution.
+     *
+     * @param e exception containing information on the unexpected error.
+     */
+    public static void showUnexpectedError(final Exception e) {
+        System.out.println(new ErrorStringBuilder(UNEXPECTED_ERROR_MESSAGE));
+        e.printStackTrace();
     }
 
+    /**
+     * This method triggers the execution of this shell,
+     * initiating a start-up procedure followed by the Read, Eval, Print loop.
+     *
+     * @param args arguments passed to the application.
+     */
     public void execute(final String[] args) {
         start(args);
 
         runREPL();
     }
 
+    /**
+     * Executes the shell's start-up procedure,
+     * which consists of displaying start-up messages
+     * and interpreting any arguments provided at application start-up.
+     *
+     * @param args arguments provided at application start-up
+     */
     private void start(final String[] args) {
         System.out.println(WELCOME_MESSAGE);
 
@@ -52,41 +109,58 @@ public final class CommandLineShell {
         } catch (FlagException fe) {
 
             System.out.println(fe.showMessage());
-            System.exit(0);
+            Runtime.getRuntime().exit(0);
         }
     }
 
+    /**
+     * Read, Eval, Print loop of this shell.
+     * It asks for input of a command or an attempt, recognises it
+     * and passes control to the responsible Controller class.
+     *
+     * @see Controller
+     */
     private void runREPL() {
-        try {
-            do {
-                System.out.println();
-                System.out.print(INPUT_PREFIX);
+        do {
+            try {
+                Command command = inputCommand();
 
-                String commandString = commandLineInputStream.readLine();
-                System.out.println();
+                Controller controller = createController(command);
 
-                try {
-                    Command command = commandParser.parse(commandString);
+                controller.control(command.getArguments());
+            } catch (InvalidCommandException e) {
+                System.out.println(
+                        new ErrorStringBuilder(UNKNOWN_COMMAND_MESSAGE));
 
-                    Controller controller = command.getCommandType().getControllerClass().getConstructor()
-                            .newInstance();
-                    controller.control(command.getArgs());
-                } catch (InvalidCommandException exception) {
-                    System.out.println(new ErrorStringBuilder(UNKNOWN_COMMAND_MESSAGE));
-                    System.out.println(HELP_TIP);
-                }
-            } while (true);
-        } catch (Exception ignored) {
+                System.out.println(HELP_TIP);
+            } catch (IOException | ReflectiveOperationException e) {
+                showUnexpectedError(e);
+            }
+        }
+        while (true);
+    }
+
+    private Command inputCommand()
+            throws InvalidCommandException, IOException {
+
+        System.out.println();
+        System.out.print(INPUT_PREFIX);
+
+        String commandString = commandLineInputStream.readLine();
+
+        System.out.println();
+
+        if (commandString != null) {
+            return commandParser.parse(commandString);
+        } else {
+            throw new NullPointerException("end of stream has been reached.");
         }
     }
 
-    private void registerCommands() {
-        
-        commandParser.addCommand("/nuova", CommandType.NUOVA);
-        commandParser.addCommand("/gioca", CommandType.GIOCA);
-        commandParser.addCommand("/abbandona", CommandType.ABBANDONA);
-        commandParser.addCommand(EXIT_MESSAGE, CommandType.ESCI);
-        commandParser.addCommand(HELP_MESSAGE, CommandType.HELP);
-        commandParser.addCommand(SHOW_MESSAGE, CommandType.MOSTRA);
+    private Controller createController(final Command command)
+            throws ReflectiveOperationException {
+
+        return command.getCommandType().getControllerClass()
+                .getConstructor().newInstance();
     }
 }
