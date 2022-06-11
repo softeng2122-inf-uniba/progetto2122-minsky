a556517b2059c7c99854338c3db30eefc30fce45
Vito Proscia <vitoproscia.1704@gmail.com>
2022-06-11 12:33:57 +0200

    Risolti warning di checktyle legati alla visualizzazione dell'help

--- src/main/java/it/uniba/app/controller/HelpController.java
+++ src/main/java/it/uniba/app/controller/HelpController.java
@@ -1,4 +1,5 @@
 package it.uniba.app.controller;
+
 import it.uniba.app.exception.FlagException;
 
 /**
@@ -9,41 +10,76 @@ import it.uniba.app.exception.FlagException;
 
 public class HelpController implements Controller {
 
-  public static void showHelp() {
+  /**
+   * Print the help message.
+   */
 
-    System.out.println("\nDESCRIZIONE APPLICAZIONE: Videogame Wordle in command line interface. \n\n" +
+  private static void showHelp() {
 
-        "Il gioco consiste nell'indovinare una parola preimpostata (parola segreta) in un massimo di 6 tentativi. \n" +
-        "Dopo ogni tentativo i colori delle lettere cambieranno per mostrare il feedback, in particolare \n\n" +
+    System.out.println(
 
-        " * VERDE rappresenta una lettera della parola segreta posizionata correttamente, \n" +
-        " * GIALLO una lettera in parola ma non nella posizione corretta, \n" +
-        " * GRIGIO una lettera non in parola. \n\n" +
-
-        "OPZIONI: \n\n" +
-
-        " -h, --help  Visualizza questa guida. \n\n" +
-
-        "COMANDI IN APP: \n\n" +
-
-        " /nuova <parola>  Imposta <parola> come nuova parola segreta. \n" +
-        " /mostra	  Visualizza la parola segreta attuale. \n" +
-        " /help		  Visualizza questa guida. \n" +
-        " /gioca 	  Inizio di una nuova partita. \n" +
-        " /abbandona	  Termina la partita correntemente in esecuzione. \n" +
-        " /esci		  Termina l'applicazione. \n"
+        "\nDESCRIZIONE APPLICAZIONE: "
+            +
+            "Videogame Wordle in command line interface. \n\n"
+            +
+            "Il gioco consiste nell'indovinare una parola preimpostata "
+            +
+            "(parola segreta) in un massimo di 6 tentativi. \n"
+            +
+            "Dopo ogni tentativo i colori delle lettere "
+            +
+            "cambieranno per mostrare il feedback, in particolare \n\n"
+            +
+            " * VERDE rappresenta una lettera della "
+            +
+            "parola segreta posizionata correttamente, \n"
+            +
+            " * GIALLO una lettera in parola ma non "
+            +
+            " nella posizione corretta, \n"
+            +
+            " * GRIGIO una lettera non in parola. \n\n"
+            +
+            "OPZIONI: \n\n"
+            +
+            " -h, --help  Visualizza questa guida. \n\n"
+            +
+            "COMANDI IN APP: \n\n"
+            +
+            " /nuova <parola>  Imposta <parola> come nuova parola segreta. \n"
+            +
+            " /mostra \t  Visualizza la parola segreta attuale. \n"
+            +
+            " /help \t\t  Visualizza questa guida. \n"
+            +
+            " /gioca \t  Inizio di una nuova partita. \n"
+            +
+            " /abbandona \t  Termina la partita correntemente in esecuzione. \n"
+            +
+            " /esci \t\t  Termina l'applicazione. \n"
 
     );
 
   }
 
-  public static boolean checkFlagOnStart(String[] args) throws FlagException {
+  /**
+   * Check if the application is invoked with the correct flegs.
+   *
+   * @param args
+   * @return helpFlag
+   * @throws FlagException
+   */
+
+  public static boolean checkFlagOnStart(final String[] args)
+      throws FlagException {
+
+    boolean helpFlag;
 
     if (args.length > 0) {
 
       if (args[0].equals("-h") || args[0].equals("--help")) {
 
-        return true;
+        helpFlag = true;
       } else {
 
         throw new FlagException();
@@ -51,12 +87,19 @@ public class HelpController implements Controller {
 
     } else {
 
-      return false;
+      helpFlag = false;
     }
+
+    return helpFlag;
 
   }
 
-  public void control(String[] args) {
+  /**
+   * Visualize the help message.
+   */
+
+  @Override
+  public void control(final String[] args) {
 
     HelpController.showHelp();
   }
