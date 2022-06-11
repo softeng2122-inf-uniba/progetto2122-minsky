db6fadf667fa3f5d5b4c0d8e6f01b80dad501b68
Vito Proscia <vitoproscia.1704@gmail.com>
2022-06-11 13:00:38 +0200

    Spostamento del metodo showHelp nella classe HelpBoundary.java

--- src/main/java/it/uniba/app/controller/HelpController.java
+++ src/main/java/it/uniba/app/controller/HelpController.java
@@ -1,6 +1,7 @@
 package it.uniba.app.controller;
 
 import it.uniba.app.exception.FlagException;
+import it.uniba.app.wordle.HelpBoundary;
 
 /**
  * <Control>
@@ -9,58 +10,6 @@ import it.uniba.app.exception.FlagException;
  */
 
 public class HelpController implements Controller {
-
-  /**
-   * Print the help message.
-   */
-
-  private static void showHelp() {
-
-    System.out.println(
-
-        "\nDESCRIZIONE APPLICAZIONE: "
-            +
-            "Videogame Wordle in command line interface. \n\n"
-            +
-            "Il gioco consiste nell'indovinare una parola preimpostata "
-            +
-            "(parola segreta) in un massimo di 6 tentativi. \n"
-            +
-            "Dopo ogni tentativo i colori delle lettere "
-            +
-            "cambieranno per mostrare il feedback, in particolare \n\n"
-            +
-            " * VERDE rappresenta una lettera della "
-            +
-            "parola segreta posizionata correttamente, \n"
-            +
-            " * GIALLO una lettera in parola ma non "
-            +
-            " nella posizione corretta, \n"
-            +
-            " * GRIGIO una lettera non in parola. \n\n"
-            +
-            "OPZIONI: \n\n"
-            +
-            " -h, --help  Visualizza questa guida. \n\n"
-            +
-            "COMANDI IN APP: \n\n"
-            +
-            " /nuova <parola>  Imposta <parola> come nuova parola segreta. \n"
-            +
-            " /mostra \t  Visualizza la parola segreta attuale. \n"
-            +
-            " /help \t\t  Visualizza questa guida. \n"
-            +
-            " /gioca \t  Inizio di una nuova partita. \n"
-            +
-            " /abbandona \t  Termina la partita correntemente in esecuzione. \n"
-            +
-            " /esci \t\t  Termina l'applicazione. \n"
-
-    );
-
-  }
 
   /**
    * Check if the application is invoked with the correct flegs.
@@ -101,7 +50,7 @@ public class HelpController implements Controller {
   @Override
   public void control(final String[] args) {
 
-    HelpController.showHelp();
+    HelpBoundary.showHelp();
   }
 
 }
