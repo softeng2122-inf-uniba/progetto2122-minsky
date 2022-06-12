2b2f09b8e654442ba62d899163c30b1d828dade7
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-12 11:47:27 +0200

    Aggiunge test case "validWordTest" a `WordTest`

--- src/test/java/it/uniba/app/wordle/WordTest.java
+++ src/test/java/it/uniba/app/wordle/WordTest.java
@@ -1,4 +1,29 @@
 package it.uniba.app.wordle;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 class WordTest {
+
+    /**
+     * String containing all accepted letters for a valid word.
+     */
+    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyz"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    @Test
+    void validWordTest() {
+        final int n = LETTERS.length();
+        final Random random = new Random();
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < Word.getLength(); i++) {
+            stringBuilder.append(LETTERS.charAt(random.nextInt(n)));
+        }
+
+        Assertions.assertDoesNotThrow(
+                () -> Word.checkWord(stringBuilder.toString()));
+    }
 }
