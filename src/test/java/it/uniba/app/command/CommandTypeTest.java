d534b2af4b9c4715b50a6fb878ad7a907cbf46e5
Mario Venezia <mvenezia00@hotmail.it>
2022-06-13 21:15:12 +0200

    Aggiunto test per CommandType.ATTEMPT.

--- src/test/java/it/uniba/app/command/CommandTypeTest.java
+++ src/test/java/it/uniba/app/command/CommandTypeTest.java
@@ -4,21 +4,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class CommandTypeTest {
-
     @Test
     @DisplayName("This test check if the CommandType enum "
             + "is correctly implemented.")
-    void testfromLabel() {
+    void fromLabel() {
         String[] commands = {"/nuova", "/mostra", "/help", "/gioca",
                 "/abbandona", "/esci"};
         CommandType[] commandlist = {CommandType.NUOVA, CommandType.MOSTRA,
                 CommandType.HELP, CommandType.GIOCA, CommandType.ABBANDONA,
-                CommandType.ESCI};
+                CommandType.ESCI, CommandType.ATTEMPT};
         int i;
         for (i = 0; i < commands.length; i++) {
             assertEquals(commandlist[i], CommandType.fromLabel(commands[i]));
         }
+        String attempt = "prova";
+        assertNull(CommandType.fromLabel(attempt));
     }
 }
