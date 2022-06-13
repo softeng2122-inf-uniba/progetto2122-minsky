6f2c1a5c3160f98fc03415729b457d2986e88639
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-13 19:49:27 +0200

    Aggiunge test case “validSecretWordTest”

--- src/test/java/it/uniba/app/wordle/SecretWordTest.java
+++ src/test/java/it/uniba/app/wordle/SecretWordTest.java
@@ -1,4 +1,38 @@
 package it.uniba.app.wordle;
 
+import it.uniba.app.exception.InvalidWordException;
+import it.uniba.app.exception.LongWordException;
+import it.uniba.app.exception.ShortWordException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
 class SecretWordTest {
+
+    @Test
+    void validSecretWordTest()
+            throws LongWordException, ShortWordException, InvalidWordException,
+            NoSuchFieldException, IllegalAccessException {
+
+        if (Word.getLength() >= 0) {
+            final String secretWordString =
+                    WordTest.randomAlphaWord(Word.getLength());
+
+            Assertions
+                    .assertDoesNotThrow(() -> new SecretWord(secretWordString));
+
+            final SecretWord secretWord = new SecretWord(secretWordString);
+            final Field secretWordStringRepresentation =
+                    secretWord.getClass().getDeclaredField("secretWord");
+
+            Assertions.assertSame(
+                    secretWordStringRepresentation.getType(), String.class);
+
+            secretWordStringRepresentation.setAccessible(true);
+
+            Assertions.assertTrue(secretWordString.equalsIgnoreCase(
+                    (String) secretWordStringRepresentation.get(secretWord)));
+        }
+    }
 }
