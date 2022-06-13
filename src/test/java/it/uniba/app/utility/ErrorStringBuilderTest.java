b6b806dd0e6ecbd86b86333d10f27f79515c3245
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-13 22:18:42 +0200

    Aggiunge test case ”emptyConstructorTest”

--- src/test/java/it/uniba/app/utility/ErrorStringBuilderTest.java
+++ src/test/java/it/uniba/app/utility/ErrorStringBuilderTest.java
@@ -1,4 +1,41 @@
 package it.uniba.app.utility;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
 class ErrorStringBuilderTest {
+
+    /**
+     * Prefix of error string messages.
+     */
+    private static String prefix;
+
+    /**
+     * Suffix of error string messages.
+     */
+    private static String suffix;
+
+    @BeforeAll
+    static void beforeAll()
+            throws NoSuchFieldException, IllegalAccessException {
+        final Field prefixField = ErrorStringBuilder.class
+                .getDeclaredField("ERROR_STRING_PREFIX");
+        final Field suffixField = ErrorStringBuilder.class
+                .getDeclaredField("ERROR_STRING_SUFFIX");
+
+        prefixField.setAccessible(true);
+        suffixField.setAccessible(true);
+
+        prefix = (String) prefixField.get(null);
+        suffix = (String) suffixField.get(null);
+    }
+
+    @Test
+    void emptyConstructorTest() {
+        Assertions.assertEquals(
+                prefix + suffix, new ErrorStringBuilder().toString());
+    }
 }
