d3784d247775a390a900fb3565891ba926aece5e
Mario Venezia <mvenezia00@hotmail.it>
2022-06-13 21:17:14 +0200

    Separati casi di test in più @test.

--- src/test/java/it/uniba/app/command/CommandTest.java
+++ src/test/java/it/uniba/app/command/CommandTest.java
@@ -9,20 +9,35 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 class CommandTest {
     @Test
-    @DisplayName("This test check if the Command methods are correctly "
-            + "implemented")
+    @DisplayName("This test check if the getCommandType "
+            + "method is correctly implemented")
     void getCommandType() {
         String[] commands = {"/nuova", "/mostra", "/help", "/gioca",
-                "/abbandona", "/esci"};
+                "/abbandona", "/esci", "prova"};
         CommandType[] commandlist = CommandType.values();
         int i;
         try {
             for (i = 0; i < commands.length; i++) {
                 Command command1 = new Command(commandlist[i], commands);
-                assertSame(commands.getClass(),
-                        command1.getArguments().getClass());
                 assertEquals(commandlist[i], command1.getCommandType());
-
+            }
+        } catch (InvalidCommandException e) {
+            System.out.println("comando non valido");
+        }
+    }
+    @Test
+    @DisplayName("This test check if the getArguments method"
+            + "is correctly implemented.")
+    void getArguments() {
+        String[] commands = {"/nuova", "/mostra", "/help", "/gioca",
+                "/abbandona", "/esci", "prova"};
+        CommandType[] commandlist = CommandType.values();
+        int i;
+        try {
+            for (i = 0; i < commands.length; i++) {
+                Command command5 = new Command(commandlist[i], commands);
+                assertSame(commands.getClass(),
+                        command5.getArguments().getClass());
             }
         } catch (InvalidCommandException e) {
             System.out.println("comando non valido");
