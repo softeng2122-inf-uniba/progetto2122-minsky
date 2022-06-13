487bfdc7fa091aa8c75ad1bcff63f71e8f6210a1
Raffaello Romanazzi <raffaello.romanazzi@gmail.com>
2022-06-13 20:22:44 +0200

    Aggiunge test case “getCurrentSecretWordTest”

--- src/test/java/it/uniba/app/wordle/SecretWordTest.java
+++ src/test/java/it/uniba/app/wordle/SecretWordTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.Random;
 
 class SecretWordTest {
+
+    /**
+     * Used to generate a random boolean in
+     * {@link SecretWordTest#getCurrentSecretWordTest()}.
+     */
+    private static final Random RANDOM = new Random();
 
     @Test
     void validSecretWordTest()
@@ -34,5 +41,26 @@ class SecretWordTest {
             Assertions.assertTrue(secretWordString.equalsIgnoreCase(
                     (String) secretWordStringRepresentation.get(secretWord)));
         }
+    }
+
+    @Test
+    void getCurrentSecretWordTest()
+            throws LongWordException, ShortWordException, InvalidWordException,
+            NoSuchFieldException, IllegalAccessException {
+
+        final Field currentSecretWord =
+                SecretWord.class.getDeclaredField("currentSecretWord");
+
+        Assertions.assertSame(currentSecretWord.getType(), SecretWord.class);
+
+        currentSecretWord.setAccessible(true);
+
+        if (RANDOM.nextBoolean() && Word.getLength() >= 0) {
+            currentSecretWord.set(null,
+                    new SecretWord(WordTest.randomAlphaWord(Word.getLength())));
+        }
+
+        Assertions.assertEquals(
+                currentSecretWord.get(null), SecretWord.getCurrentSecretWord());
     }
 }
